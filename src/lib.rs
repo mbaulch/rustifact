04bc470febad322d20e6e28d24d43e1bdac96fd3
Matthew Baulch <baulch.matt@gmail.com>
2023-06-12 13:30:57 +1000

    Fix a mild formatting error.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.//! # Rustifact
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+//! # Rustifact
 //!
 //! _A seamless bridge between a build script and the main crate._
 //!
