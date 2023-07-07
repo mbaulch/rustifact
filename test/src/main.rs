63c6beb4d5f4bbb24fea6cf2de061500e0ae4125
Matthew Baulch <baulch.matt@gmail.com>
2023-07-08 00:06:20 +1000

    Implement minimal functionality for test framework and a basic proof-of-concept test

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,3 +1,84 @@
+use std::fs::{self, File};
+use std::io::{self, BufRead, Write};
+use std::path::{Path, PathBuf};
+use std::process::Command;
+use walkdir::WalkDir;
+
 fn main() {
-    todo!();
+    let output_dir = Path::new("target/test");
+    // Prepare the test output directory
+    if output_dir.exists() {
+        fs::remove_dir_all(&output_dir).expect("Failed to remove existing test directory");
+    }
+    fs::create_dir_all(&output_dir).expect("Failed to create test directory");
+    for entry in WalkDir::new("test") {
+        let entry = entry.expect("Failed to read directory entry");
+        let path = entry.path();
+        if path.is_file() && path.extension() == Some("test".as_ref()) {
+            run_test(path, &output_dir);
+        }
+    }
+}
+
+fn run_test(input_path: &Path, output_dir: &Path) {
+    // Prepare the output dir with the files specified in the file at input_path
+    if !parse_and_write_files(input_path, &output_dir).is_ok() {
+        panic!("Failed to create files for test {}", input_path.display());
+    }
+    let cargo_run_output = Command::new("cargo")
+        .arg("run")
+        .arg("-q")
+        .current_dir(&output_dir)
+        .status()
+        .expect("failed to run test with 'cargo run'");
+
+    if cargo_run_output.success() {
+        println!("***** {} PASS", input_path.display());
+    } else {
+        println!("***** {} FAIL", input_path.display());
+    }
+}
+
+fn parse_and_write_files(source_path: &Path, out_prefix: &Path) -> io::Result<()> {
+    let source_file = File::open(source_path)?;
+    let reader = io::BufReader::new(source_file);
+
+    let mut current_file: Option<File> = None;
+
+    for line in reader.lines() {
+        let line = line?;
+        if line.starts_with("//file:") {
+            // Close previous file
+            if let Some(mut file) = current_file.take() {
+                file.flush()?;
+            }
+            // Open new file
+            let file_name = line.trim_start_matches("//file:").trim();
+            if file_name.starts_with("..") {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "File path cannot start with '..'",
+                ));
+            }
+            let file_path = out_prefix.join(Path::new(file_name));
+            // Create directories if necessary
+            let path = PathBuf::from(&file_path);
+            if let Some(parent) = path.parent() {
+                if !parent.exists() {
+                    fs::create_dir_all(parent)?;
+                }
+            }
+            // Create the file
+            current_file = Some(File::create(&file_path)?);
+        } else if let Some(file) = current_file.as_mut() {
+            writeln!(file, "{}", line)?;
+        }
+    }
+
+    // Flush and close the last file
+    if let Some(mut file) = current_file.take() {
+        file.flush()?;
+    }
+
+    Ok(())
 }
